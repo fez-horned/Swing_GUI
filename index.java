af03811758f6f43eaeba1345e0c7708b9098517c
Sarah <153827773+fez-horned@users.noreply.github.com>
2023-12-18 01:14:42 +0100

    Create index.java

---
+++ index.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+import java.awt.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DigitalClock extends JFrame {
+    // ... (digital clock GUI and logic)
+}
